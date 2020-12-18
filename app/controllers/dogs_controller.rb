9c58d62d066cd2c9182860d5dc29760ba56523e2
danajackson2 <danajackson2@gmail.com>
2020-12-18 14:04:13 -0800

    how do i make the sorted dogs an option??

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,10 @@
 class DogsController < ApplicationController
+    def index
+        @dogs = Dog.all
+        @dogs_sorted = @dogs.sort_by{|dog|dog.employees.length}.reverse
+    end
+
+    def show
+        @dog = Dog.find(params[:id])
+    end
 end
